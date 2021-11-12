d709bdae6e83244bde2697467925547320305e49
Sivajone <sivajone@protonmail.com>
2021-11-12 22:01:22 +0100

    app routes change

--- app.py
+++ app.py
@@ -5,21 +5,20 @@ from pytube import YouTube
 app = Flask(__name__)
 
 
-@app.route('/', methods=['POST', 'GET'])
+@app.route('/')
+def redirect():
+    return redirect('https://ytmp3.sivaj.pl/')
+
+
+@app.route('/download',)
 def download():
-    if request.method == 'GET':
-        return jsonify(
-            status='405',
-        ), 405
+    url = request.form.get('url')
+    print(url)
+    video = YouTube(url)
+    stream = video.streams.order_by('abr').get_audio_only()
+    filename = stream.default_filename.replace(' ', '-')
+    stream.download(output_path='./static/', filename=filename)
 
-    if request.method == 'POST':
-        url = request.form['url']
-        print(url)
-        video = YouTube(url)
-        stream = video.streams.order_by('abr').get_audio_only()
-        filename = stream.default_filename.replace(' ', '-')
-        stream.download(output_path='./static/', filename=filename)
-
-        return jsonify(
-            url='/static/' + filename
-        )
+    return jsonify(
+        url='/static/' + filename
+    )
